88a2c74a99fc2cb21afee1529b1fc66218ace683
ryu-f67 <161839926+ryu-f67@users.noreply.github.com>
2024-07-13 08:02:05 +0900

    CRUD処理追加

--- src/main/java/student/management/StudentManagement/StudentManagementApplication.java
+++ src/main/java/student/management/StudentManagement/StudentManagementApplication.java
@@ -1,55 +1,51 @@
 package student.management.StudentManagement;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
-	private String name = "Ryuya";
-	private String age = "30";
-
-	private Map<String, String> favorite = new HashMap<>();
+	@Autowired
+	private StudentRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
-	@GetMapping("/studentInfo")
-	public String getStudentInfo() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("I am "+ name +"," + age + " years old.");
-		sb.append(System.lineSeparator());
-		if (!favorite.isEmpty()) {
-			favorite.forEach((key, value) -> sb.append("My favorite " + key + " is " + value + "." + System.lineSeparator()));
-		}
-		return sb.toString();
+	// テーブル内すべてを取得
+	@GetMapping("/studentTable")
+	public List<Student> showStudentTable() {
+		return repository.showStudentTable();
 	}
 
-	@PostMapping("/studentInfo")
-	public void postStudentInfo(String name, String age) {
-		this.name = name;
-		this.age = age;
+	@GetMapping("/student")
+	public String getStudent(@RequestParam String name) {
+		Student student = repository.searchByName(name);
+		return student.getName() + " " + student.getAge() + "歳 " + student.getBloodType() + "型";
 	}
 
-	@PostMapping("/age")
-	public void updateAge(String age) {
-		this.age = age;
+	@PostMapping("/student")
+	public void registerStudent(String name, int age, String bloodType) {
+		repository.registerStudent(name, age, bloodType);
 	}
 
-	@PostMapping("/name")
-	public void updateName(String name) {
-		this.name = name;
+	@PatchMapping("/student")
+	public void updateStudent(@RequestParam String name, int age, String bloodType) {
+		repository.updateStudent(name, age, bloodType);
 	}
 
-	@PostMapping("/favorite")
-	public void addFavorite(String category, String things) {
-		this.favorite.put(category, things);
+	@DeleteMapping("/student")
+	public void deleteStudent(String name) {
+		repository.deleteStudent(name);
 	}
 }
