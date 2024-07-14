e50e9dd7754cb6db346a6c29f141981730bffd3a
ryu-f67 <161839926+ryu-f67@users.noreply.github.com>
2024-07-14 19:04:23 +0900

    パッケージの移動

---
+++ src/main/java/student/management/StudentManagement/repository/StudentRepository.java
@@ -0,0 +1,17 @@
+package student.management.StudentManagement.repository;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import student.management.StudentManagement.data.Student;
+import student.management.StudentManagement.data.StudentCourse;
+
+@Mapper
+public interface StudentRepository {
+
+  @Select("SELECT * FROM students")
+  List<Student> search();
+
+  @Select("SELECT * FROM students_courses")
+  List<StudentCourse> searchStudentCourses();
+}
