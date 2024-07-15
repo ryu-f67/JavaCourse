8699d2060697a64c3dc3fba8871f06ebfba521a8
ryu-f67 <161839926+ryu-f67@users.noreply.github.com>
2024-07-15 10:13:33 +0900

    converter追加

--- src/main/java/student/management/StudentManagement/controller/StudentController.java
+++ src/main/java/student/management/StudentManagement/controller/StudentController.java
@@ -4,24 +4,30 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import student.management.StudentManagement.controller.converter.StudentConverter;
 import student.management.StudentManagement.data.Student;
 import student.management.StudentManagement.data.StudentCourse;
+import student.management.StudentManagement.domain.StudentDetail;
 import student.management.StudentManagement.service.StudentService;
 
 @RestController
-public class StudentContoroller {
+public class StudentController {
 
   private StudentService service;
+  private StudentConverter converter;
 
   @Autowired
-  public StudentContoroller(StudentService service) {
+  public StudentController(StudentService service, StudentConverter converter) {
     this.service = service;
+    this.converter = converter;
   }
 
-  // studentsテーブル内すべてを取得
   @GetMapping("/studentsList")
-  public List<Student> getStudentList() {
-    return service.searchStudentList();
+  public List<StudentDetail> getStudentList() {
+    List<Student> students = service.searchStudentList();
+    List<StudentCourse> studentCourses = service.searchStudentCourseList();
+
+    return converter.convertStudentDetails(students, studentCourses);
   }
 
   // students_coursesテーブル内すべてを取得
