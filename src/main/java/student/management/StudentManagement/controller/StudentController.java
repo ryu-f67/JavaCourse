52165b218893ba5ee943e32cbdc98a295c317a13
ryu-f67 <161839926+ryu-f67@users.noreply.github.com>
2024-07-18 18:50:33 +0900

    受講生登録処理追加

--- src/main/java/student/management/StudentManagement/controller/StudentController.java
+++ src/main/java/student/management/StudentManagement/controller/StudentController.java
@@ -4,10 +4,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import student.management.StudentManagement.controller.converter.StudentConverter;
 import student.management.StudentManagement.data.Student;
 import student.management.StudentManagement.data.StudentCourse;
+import student.management.StudentManagement.domain.StudentDetail;
 import student.management.StudentManagement.service.StudentService;
 
 @Controller
@@ -38,6 +42,32 @@ public class StudentController {
 
     model.addAttribute("studentsCourseList", studentCourses);
     return "studentsCourseList";
+  }
+
+  @GetMapping("/newStudent")
+  public String newStudent(Model model) {
+    model.addAttribute("studentDetail", new StudentDetail());
+    return "registerStudent";
+  }
+
+  @PostMapping("/registerStudent")
+  public String registerStudent(@ModelAttribute StudentDetail studentDetail, BindingResult result) {
+    if (result.hasErrors()) {
+      return "registerStudent";
+    }
+    System.out.println("ID:" + studentDetail.getStudent().getId());
+    System.out.println("名前:" + studentDetail.getStudent().getName());
+    System.out.println("ふりがな:" + studentDetail.getStudent().getFurigana());
+    System.out.println("ニックネーム:" + studentDetail.getStudent().getNickname());
+    System.out.println("メールアドレス:" + studentDetail.getStudent().getMail());
+    System.out.println("地域:" + studentDetail.getStudent().getArea());
+    System.out.println("年齢:" + studentDetail.getStudent().getAge());
+    System.out.println("性別:" + studentDetail.getStudent().getGender());
+    System.out.println("備考:" + studentDetail.getStudent().getRemark());
+
+    service.registerStudent(studentDetail);
+
+    return "redirect:/studentsList";
   }
 
 }
--- src/main/java/student/management/StudentManagement/repository/StudentRepository.java
+++ src/main/java/student/management/StudentManagement/repository/StudentRepository.java
@@ -1,6 +1,7 @@
 package student.management.StudentManagement.repository;
 
 import java.util.List;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import student.management.StudentManagement.data.Student;
@@ -14,4 +15,10 @@ public interface StudentRepository {
 
   @Select("SELECT * FROM students_courses")
   List<StudentCourse> searchStudentCourses();
+
+  @Insert(
+      "INSERT students(id, name, furigana, nickname, mail, area, age, gender, remark, is_deleted)"
+          + "VALUES(#{id}, #{name}, #{furigana}, #{nickname}, #{mail}, #{area}, #{age}, #{gender}, #{remark}, false)")
+  void insert(Student student);
+
 }
