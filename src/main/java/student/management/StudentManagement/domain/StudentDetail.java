95954bdac36857a6a9fe107656f32b155387ed52
ryu-f67 <161839926+ryu-f67@users.noreply.github.com>
2024-08-18 14:46:08 +0900

    アノテーション追加

--- src/main/java/student/management/StudentManagement/domain/StudentDetail.java
+++ src/main/java/student/management/StudentManagement/domain/StudentDetail.java
@@ -1,13 +1,17 @@
 package student.management.StudentManagement.domain;
 
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import student.management.StudentManagement.data.Student;
 import student.management.StudentManagement.data.StudentCourse;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class StudentDetail {
 
   private Student student;
