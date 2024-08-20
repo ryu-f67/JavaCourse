2a99e585c18261782d7ce34a4bafddf0208cd10e
ryu-f67 <161839926+ryu-f67@users.noreply.github.com>
2024-08-20 21:37:40 +0900

    SQL文をxmlファイルに追加

--- src/main/java/student/management/StudentManagement/repository/StudentRepository.java
+++ src/main/java/student/management/StudentManagement/repository/StudentRepository.java
@@ -45,8 +45,7 @@ public interface StudentRepository {
    *
    * @return 受講生コース情報(全件)
    */
-  @Select("SELECT * FROM students_courses")
-  List<StudentCourse> searchStudentCourseList();
+  List<StudentCourse> searchAllStudentCourseList();
 
   /**
    * 受講生IDに紐づく受講生コース情報を検索します。
@@ -54,17 +53,13 @@ public interface StudentRepository {
    * @param studentId 受講生ID
    * @return 受講生IDに紐づく受講生コース情報
    */
-  @Select("SELECT * FROM students_courses WHERE student_id=#{studentId}")
-  List<StudentCourse> searchStudentCourse(String studentId);
+  List<StudentCourse> searchStudentCourseList(String studentId);
 
   /**
    * 受講生情報を登録します。
    *
    * @param student 受講生情報
    */
-  @Insert(
-      "INSERT students(id, name, furigana, nickname, mail, area, age, gender, remark, is_deleted)"
-          + "VALUES(#{id}, #{name}, #{furigana}, #{nickname}, #{mail}, #{area}, #{age}, #{gender}, #{remark}, false)")
   void insert(Student student);
 
   /**
@@ -72,9 +67,6 @@ public interface StudentRepository {
    *
    * @param studentCourse 受講生コース情報
    */
-  @Insert(
-      "INSERT students_courses(student_id, course_name, start_at, end_at)"
-          + "VALUES(#{studentId}, #{courseName}, #{startAt}, #{endAt})")
   @Options(useGeneratedKeys = true, keyProperty = "id")
   void insertCourse(StudentCourse studentCourse);
 
@@ -83,10 +75,6 @@ public interface StudentRepository {
    *
    * @param student 受講生
    */
-  @Update(
-      "UPDATE students SET name=#{name}, furigana=#{furigana}, nickname=#{nickname}, mail=#{mail}, "
-          + "area=#{area}, age=#{age}, gender=#{gender}, remark=#{remark}, is_deleted=#{isDeleted} "
-          + "WHERE id = #{id}")
   void updateStudent(Student student);
 
   /**
@@ -94,8 +82,6 @@ public interface StudentRepository {
    *
    * @param studentCourse 受講生コース情報(コース名)
    */
-  @Update(
-      "UPDATE students_courses SET course_name=#{courseName} WHERE id = #{id}")
   @Options(useGeneratedKeys = true, keyProperty = "id")
   void updateStudentCourse(StudentCourse studentCourse);
 
